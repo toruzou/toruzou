a4f0405357597119074bcb65d1ea78155bd66874
monzou <1984tkr@gmail.com>
2013-07-28 12:46:20 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toruzou::Application.config.secret_key_base = 'a75db0158fd7f2773b2d4ce09c0802becc24934bd344e6f724d3fbe542c86dc39ee60ebcd11a568b582e3d74d4283d94138cc1b689f0d7e1682d41d31adb31b3'
