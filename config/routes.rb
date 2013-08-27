298afb19fb23f1ef3aa33e6c903f9f4b237a0bf2
monzou <1984tkr@gmail.com>
2013-08-27 22:26:04 +0900

    Add route to Contacts, Organization and Person.

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Toruzou::Application.routes.draw do
     end
   end
 
-  # Other API Controllers should be placed here.
-  # namespace :api do
-  #   namespace :v1 do
-  #     devise_for :users
-  #   end
-  # end
+  namespace :api, defaults: { format: "json" } do
+    namespace :v1 do
+      resources :contacts
+      resources :organizations
+      resources :people
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
