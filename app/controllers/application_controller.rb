e0ff869ea16daa068892cfeadfd5948b45721d79
monzou <1984tkr@gmail.com>
2013-08-27 22:28:23 +0900

    Enable to respond with the pageable collection :heartbeat:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base  
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def to_pageable_collection(collection)
+    [ { :total_entries => collection.count }, collection ]
+  end
+
 end
