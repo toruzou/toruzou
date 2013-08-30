86efc04e72d96e5d44e1b3a03c800a317a834a66
monzou <1984tkr@gmail.com>
2013-08-30 13:03:25 +0900

    Fix permission to Users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password ) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password) }
   end
 
 end
