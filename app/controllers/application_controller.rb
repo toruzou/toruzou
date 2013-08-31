75afbfea65ea0f0da8538a93ea98ca0bbd16d31b
monzou <1984tkr@gmail.com>
2013-08-31 13:16:09 +0900

    Improve method name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,21 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  protected
 
   def to_pageable_collection(collection)
     [ { :total_entries => collection.count }, collection ]
   end
 
-  protected
-
-  def configure_permitted_parameters
+  def configure_devise_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password) }
+  end
+
+  def escape_like(s)
+    s.gsub(/\\/, "\\\\").gsub(/%/, "\\%").gsub(/_/, "\\_")
   end
 
 end
