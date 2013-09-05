1dbf7a91b016aa287239cc2331b0dc18f1cde789
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-09-05 23:58:57 +0900

    fixed activities routing test

--- spec/routing/activities_routing_spec.rb
+++ spec/routing/activities_routing_spec.rb
@@ -1,34 +1,34 @@
 require "spec_helper"
 
-describe ActivitiesController do
+describe Api::V1::ActivitiesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/activities").should route_to("activities#index")
+      get("/api/v1/activities").should route_to("api/v1/activities#index", format: 'json')
     end
 
     it "routes to #new" do
-      get("/activities/new").should route_to("activities#new")
+      get("/api/v1/activities/new").should route_to("api/v1/activities#new", format: 'json')
     end
 
     it "routes to #show" do
-      get("/activities/1").should route_to("activities#show", :id => "1")
+      get("/api/v1/activities/1").should route_to("api/v1/activities#show", :id => "1", format: 'json')
     end
 
     it "routes to #edit" do
-      get("/activities/1/edit").should route_to("activities#edit", :id => "1")
+      get("/api/v1/activities/1/edit").should route_to("api/v1/activities#edit", :id => "1", format: 'json')
     end
 
     it "routes to #create" do
-      post("/activities").should route_to("activities#create")
+      post("/api/v1/activities").should route_to("api/v1/activities#create", format: 'json')
     end
 
     it "routes to #update" do
-      put("/activities/1").should route_to("activities#update", :id => "1")
+      put("/api/v1/activities/1").should route_to("api/v1/activities#update", :id => "1", format: 'json')
     end
 
     it "routes to #destroy" do
-      delete("/activities/1").should route_to("activities#destroy", :id => "1")
+      delete("/api/v1/activities/1").should route_to("api/v1/activities#destroy", :id => "1", format: 'json')
     end
 
   end
