b05c0765a3671b193d52797174c41d74994e3660
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-09-06 00:10:19 +0900

    added organizations routing test

---
+++ spec/routing/organizations_routing_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe Api::V1::OrganizationsController do
+  describe "routing" do
+
+    it "routes to #index" do
+      get("/api/v1/updates").should route_to("api/v1/updates#index", format: 'json')
+    end
+
+    it "routes to #new" do
+      get("/api/v1/updates/new").should route_to("api/v1/updates#new", format: 'json')
+    end
+
+    it "routes to #show" do
+      get("/api/v1/updates/1").should route_to("api/v1/updates#show", :id => "1", format: 'json')
+    end
+
+    it "routes to #edit" do
+      get("/api/v1/updates/1/edit").should route_to("api/v1/updates#edit", :id => "1", format: 'json')
+    end
+
+    it "routes to #create" do
+      post("/api/v1/updates").should route_to("api/v1/updates#create", format: 'json')
+    end
+
+    it "routes to #update" do
+      put("/api/v1/updates/1").should route_to("api/v1/updates#update", :id => "1", format: 'json')
+    end
+
+    it "routes to #destroy" do
+      delete("/api/v1/updates/1").should route_to("api/v1/updates#destroy", :id => "1", format: 'json')
+    end
+
+  end
+end
