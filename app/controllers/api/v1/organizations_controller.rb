888c9fc71f199c7e644cba790a23106915033e03
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-09-12 00:14:31 +0900

    added delete and update test
    move process of record_not_found case in ApplicationController

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -18,11 +18,7 @@ module Api
 
       # GET /organizations/1
       def show
-        if @organization != nil then
-          render json: @organization
-        else
-          head :not_found
-        end
+        render json: @organization
       end
 
       # GET /organizations/new
@@ -63,18 +59,13 @@ module Api
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_organization
-          begin
-            @organization = Organization.find(params[:id])
-          rescue 
-             @organization = nil
-          end
+          @organization = Organization.find(params[:id])
         end
 
         # Only allow a trusted parameter "white list" through.
         def organization_params
           params.require(:organization).permit(:name, :abbreviation, :address, :remarks, :url, :owner_id)
         end
-        
     end
 
   end
