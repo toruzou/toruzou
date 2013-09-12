2df1dff9f2224d79cb08b8999c9f67f6c424c013
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-09-12 23:59:43 +0900

    add organization validation and test code.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,12 @@
 class Organization < Contact
   has_many :deals
   has_many :activities
+  has_many :people
+
+  validates :name, :abbreviation, presence: true
+  validates :name, length: {maximum: 80}
+  validates :abbreviation, length: {maximum: 20}
+  validates :address, length: {maximum: 200}
+  validates :remarks, length: {maximum: 200}
+  validates :url, length: {maximum: 100}
 end
