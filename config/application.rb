5a685e378cdeaef529c993c7fb7940d160e36160
monzou <1984tkr@gmail.com>
2013-09-26 01:06:01 +0900

    Embed fonts for internal network

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Toruzou
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join("vendor", "assets", "components")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.to_prepare do
       Devise::SessionsController.skip_before_action :authenticate_user!
       DeviseController.respond_to :html, :json
